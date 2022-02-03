e6ade7a127015d1db31daf6f59ad5ba46ecad815
Alex Schwartz <alexschwartz01@gmail.com>
2022-02-03 16:19:28 -0500

    Stay logged in even after reloading

--- mysite/frontend/src/index.js
+++ mysite/frontend/src/index.js
@@ -12,42 +12,21 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        const loggedInUser = localStorage.getItem("user");
-        var myUser;
-        if(loggedInUser) {
-            myUser = JSON.parse(loggedInUser);
-            if(myUser.loggedIn === true) {
-                this.state = {
-                    goToLogin: myUser.goToLogin,
-                    goToRegister: myUser.goToRegister,
-                    loggedIn: myUser.loggedIn,
-                    registeredSuccess: false,
-                    userData: {
-                        email: "",
-                        first_name: "",
-                        last_name: "",
-                        password: "",
-                        username: myUser.username,
-                    },
-                    postData: {}
-                };
-            } else {
-                this.state = {
-                    goToLogin: false,
-                    goToRegister: false,
-                    loggedIn: false,
-                    registeredSuccess: false,
-                    userData: {},
-                    postData: {}
-                };
-            }
+        const username = localStorage.getItem("username");
+        if(username) {
+            this.state = {
+                goToLogin: false,
+                goToRegister: false,
+                loggedIn: true,
+                registeredSuccess: false,
+                postData: {}
+            };
         } else {
             this.state = {
                 goToLogin: false,
                 goToRegister: false,
                 loggedIn: false,
                 registeredSuccess: false,
-                userData: {},
                 postData: {}
             };
         }
@@ -66,24 +45,14 @@ class App extends Component {
         }
     }
 
-    finishLogin(userData) {
-        localStorage.setItem('user',JSON.stringify({
-            goToLogin: false,
-            goToRegister: false,
-            username: userData.username,
-            loggedIn: true
-        }))
-        this.setState({goToLogin: false, goToRegister: false, loggedIn: true, userData});
+    finishLogin(username) {
+        localStorage.setItem('username',username);
+        this.setState({goToLogin: false, goToRegister: false, loggedIn: true});
     }
 
     finishLogout() {
-        localStorage.setItem('user',JSON.stringify({
-            goToLogin: false,
-            goToRegister: false,
-            username: '',
-            loggedIn: false
-        }))
-        this.setState({goToLogin: false, goToRegister: false, loggedIn: false, userData: {}, postData: {}});
+        localStorage.setItem('username','');
+        this.setState({goToLogin: false, goToRegister: false, loggedIn: false, postData: {}});
     }
 
     makeRequest(newData) {
@@ -101,7 +70,7 @@ class App extends Component {
                     <div>
                         <LogoutButton handleClick={this.finishLogout.bind(this)} />
                         <MakePosts
-                            username={this.state.userData.username}
+                            username={localStorage.getItem("username")}
                             makeRequest={this.makeRequest.bind(this)}
                         />
                         <Wall postData={this.state.postData} />
