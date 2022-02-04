adaec58e3a52b7eb93020a3e684e2d3cc6072a33
Alex Schwartz <alexschwartz01@gmail.com>
2022-02-04 10:29:21 -0500

    Write unit tests for APIs

--- backend/posts/tests.py
+++ backend/posts/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from .models import Post
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+post_data = {
+    "title": "Fake Title",
+    "content": "Fake message",
+    "username": "fakeuser"
+}
+
+class CreatePostTest(APITestCase):
+    def test_can_create_post(self):
+        response = self.client.post('/posts/', post_data)
+        self.assertEqual(response.status_code, 201)
+
+class ViewPostsTest(APITestCase):
+    def setUp(self):
+        self.post = Post(post_data)
+
+    def test_can_view_post_list(self):
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_view_user(self):
+        response = self.client.get('/posts/', args=[self.post.id])
+        self.assertEqual(response.status_code, 200)
--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from .models import User
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+user_data = {
+    "username": "fakeuser",
+    "first_name": "Fake",
+    "last_name": "User",
+    "email": "fake@user.com",
+    "password": "pass"
+}
+
+class CreateUserTest(APITestCase):
+    def test_can_create_user(self):
+        response = self.client.post('/users/', user_data)
+        self.assertEqual(response.status_code, 201)
+
+class ViewUsersTest(APITestCase):
+    def setUp(self):
+        self.user = User(user_data)
+
+    def test_can_view_user_list(self):
+        response = self.client.get('/users/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_view_user(self):
+        response = self.client.get('/users/', args=[self.user.id])
+        self.assertEqual(response.status_code, 200)
